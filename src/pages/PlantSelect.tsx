c364d6d7c3f38df2a89b5eb7794720606a4a31c2
Jackson Passos <jacksonguitar33@gmail.com>
2021-04-25 11:03:32 -0300

    list tags components

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
 import colors from '../styles/colors'
 
 import { Header } from '../components/Header'
 import fonts from '../styles/fonts'
+import { EnviromentButton } from '../components/EnviromentButton'
 
 export function PlantSelect() {
     return (
@@ -17,6 +18,21 @@ export function PlantSelect() {
                 <Text style={styles.subTitle}>
                     você quer colocar sua planta?
                 </Text>
+            </View>
+
+            <View>
+                <FlatList
+                    data={[1,2,3,4,5]}
+                    renderItem={({ item }) => (
+                        <EnviromentButton 
+                            title="Cozinha" 
+                            active
+                        />
+                    )}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    contentContainerStyle={styles.enviromentList}
+                />
             </View>
         </View>
     )
@@ -47,7 +63,13 @@ const styles = StyleSheet.create({
         lineHeight: 23,
         color: colors.heading,
         fontFamily: fonts.text
-
+    },
+    enviromentList: {
+        height: 40,
+        marginLeft: 32,
+        marginBottom: 5,
+        justifyContent: 'center',
+        marginVertical: 32
     }
 
 
